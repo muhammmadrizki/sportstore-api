7e447191bd927faffd5fdd6d64ad42711f1732bc
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-08-07 10:28:15 +0700

    added data on product.ts and seed

---
+++ prisma/data/products.ts
@@ -0,0 +1,60 @@
+type SeedProduct = {
+  slug: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+};
+
+export const dataSeedProducts: SeedProduct[] = [
+  {
+    slug: "mu-home-jersey-2025",
+    name: "Manchester United Home Jersey 2025",
+    description: "Official Manchester United home jersey for the 2025 season",
+    imageUrl:
+      "https://mufc-live.cdn.scayle.cloud/images/139fbaa87bc74a30fa9de0bb0c14cd9d.jpg?brightness=1&width=584&height=779&quality=70&bg=ffffff",
+    price: 1938100,
+  },
+  {
+    slug: "mu-away-jersey-2025",
+    name: "Manchester United Away Jersey 2025",
+    description: "Official Manchester United away jersey for the 2025 season",
+    imageUrl:
+      "https://mufc-live.cdn.scayle.cloud/images/d331a1fb4396017cd187e4154da95840.jpg?brightness=1&width=584&height=779&quality=70&bg=ffffff",
+    price: 1938100,
+  },
+  {
+    slug: "mu-keychain-white",
+    name: "MU keychain",
+    description:
+      "Stylish and durable keychain featuring the iconic Manchester United crest. Perfect for showing your Red Devils pride wherever you go",
+    imageUrl: "https://link-gambar.com/mu-jacket.jpg",
+    price: 1099000,
+  },
+  {
+    slug: "mu-training-short-wear",
+    name: "MU training wear short",
+    description: "Official 2025 season short training wear",
+    imageUrl:
+      "https://mufc-live.cdn.scayle.cloud/images/d6415aec7046065b8bd7017c04c16335.jpg?brightness=1&width=584&height=779&quality=70&bg=ffffff",
+    price: 866500,
+  },
+  {
+    slug: "mu-tshirt-black",
+    name: "MU x Adidas Futur Icons Black",
+    description:
+      "Casual MU shirt for everyday wear. Simple, comfy, and full of pride",
+    imageUrl:
+      "https://mufc-live.cdn.scayle.cloud/images/499261d4bd317b576a13d2d1fcf20ce5.jpg?brightness=1&width=584&height=779&quality=70&bg=ffffff",
+    price: 752500,
+  },
+  {
+    slug: "mu-waynerooney-book",
+    name: "Wayne Rooney, Teenage Kicks Book",
+    description: " Manchester united player wayne rooney book",
+    imageUrl:
+      "https://mufc-live.cdn.scayle.cloud/images/eda92dc66626b0b3fae93b64c8118abb.jpg?brightness=1&width=922&height=1230&quality=70&bg=ffffff",
+    price: 456100,
+  },
+  // Tambahkan produk lainnya di sini
+];
