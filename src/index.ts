8dbffe120f10b7ad450e49588e3b8a5561be3822
M Haidar Hanif <git@mhaidarhanif.com>
2025-10-12 21:04:52 +0700

    feat: hono logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { cors } from "hono/cors";
 import { Scalar } from "@scalar/hono-api-reference";
+import { logger } from "hono/logger";
 
 import { productRoute } from "./modules/product/route";
 import { userRoute } from "./modules/user/route";
@@ -9,6 +10,7 @@ import { cartRoute } from "./modules/cart/route";
 
 const app = new OpenAPIHono();
 
+app.use(logger());
 app.use(cors());
 app.route("/products", productRoute);
 app.route("/users", userRoute);
