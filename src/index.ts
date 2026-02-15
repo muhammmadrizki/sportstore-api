69c19b8d98ff028a16218ffa51c4b800fbe7e7dc
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2026-02-15 21:55:30 +0700

    fix: add localhost:3000 to CORS allowed origins

--- src/index.ts
+++ src/index.ts
@@ -105,6 +105,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:3000",
       "https://sportstore.muhammadrizkikurniaputra.com",
     ],
     credentials: true,
